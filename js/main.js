54421cf3e4120d788ba04eab465dc89b150edc18
Rnasc07 <raphael.nsc.job@gmail.com>
2022-02-02 15:35:08 -0300

    Testes nova funcionalidade de leitura

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+/*
+Função responsável por setar a cor do item conferido
+*Os itens terão um id único, exemplo: data-return-{numId}
+*/
+function setCheckSuccessItem(elementName){
+    let handleEl = document.getElementById(elementName);
+    handleEl.style.backgroundColor = 'rgb(220,255,244)';
+    handleEl.style.color = 'rgba(22,163,74, 0.9)';
+}
+
+//setCheckSuccessItem('data-return-451');
+// setCheckSuccessItem('data-return-452');
+// setCheckSuccessItem('data-return-453');
